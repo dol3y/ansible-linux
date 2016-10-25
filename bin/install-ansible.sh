171338d5eadba2c70e94a95ae36edd43c78eef80
Andrew Dolby <dolby.am@gmail.com>
2016-10-25 13:52:33 +0100

    add missing dep

--- bin/install-ansible.sh
+++ bin/install-ansible.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get install python-pip libssl-dev
+sudo apt-get install python-pip libssl-dev libffi-dev
 sudo -H pip install paramiko PyYAML Jinja2 httplib2 six ansible
