f523c6b4447671aafe2a51d59136f2f65e79f9f0
Andrew Dolby <dolby.am@gmail.com>
2017-02-12 21:00:30 +0000

    pin version

--- bin/install-ansible.sh
+++ bin/install-ansible.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt-get install python-pip libssl-dev libffi-dev
-sudo -H pip install paramiko PyYAML Jinja2 httplib2 six ansible
+sudo -H pip install\
+  paramiko\
+  PyYAML\
+  Jinja2\
+  httplib2\
+  six\
+  ansible==2.2.0.0
