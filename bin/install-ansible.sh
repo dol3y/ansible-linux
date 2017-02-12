71d8c74fab4671630b62706e755b33919a85df6a
Andrew Dolby <dolby.am@gmail.com>
2017-02-12 21:17:59 +0000

    again...

--- bin/install-ansible.sh
+++ bin/install-ansible.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install libffi6 libssl1.0.0
+#sudo apt-get install libffi6 libssl1.0.0
 sudo apt-get install python-pip libssl-dev libffi-dev
 sudo -H pip install\
   paramiko\
