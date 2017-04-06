dceb25b97dc018e17f5b13caec5ee27c55a6fe60
Andrew Dolby <dolby.am@gmail.com>
2017-04-06 19:59:05 +0100

    bump ansible

--- bin/install-ansible.sh
+++ bin/install-ansible.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#sudo apt-get install libffi6 libssl1.0.0
-#sudo apt-get install python-pip libssl-dev libffi-dev
-sudo -H pip install\
+sudo apt-get install libffi6 libssl1.0.0
+sudo apt-get install python-pip libssl-dev libffi-dev
+sudo -H pip2 install\
   paramiko\
   PyYAML\
   Jinja2\
   httplib2\
   six\
-  ansible==2.2.0.0
+  ansible==2.2.1.0
