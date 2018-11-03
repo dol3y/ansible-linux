786305ffe3eae19638656bd003a19d8e1314bcaf
Andrew Dolby <dolby.am@gmail.com>
2018-11-03 22:58:43 +0000

    cleanup

--- bin/install-ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIRNAME=$(dirname $0)
-
-sudo apt-get update
-sudo apt-get -y install libffi6 libssl1.0.0
-sudo apt-get -y install python-pip libssl-dev libffi-dev
-sudo -H pip2 install --upgrade setuptools
-sudo -H pip2 install -r $DIRNAME/requirements.txt
